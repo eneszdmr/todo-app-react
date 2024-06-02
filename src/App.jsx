b23f299ef274198b4a33049f4411eefecb182190
enes <derpegu@gmail.com>
2024-06-02 19:13:06 +0300

    second part committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { useState } from 'react'
 import './App.css'
 import TodoCreate from './components/TodoCreate'
 import TodoList from './components/TodoList'
 
 function App() {
 
+  const [todos, setTodos] = useState([]);
 
+  const createTodo = (todo) => {
+    setTodos([...todos, todo]);
+  }
+
+
+  console.log("first todo", todos)
+  
   return (
     <div className='App'>
       <div className='create-body '>
-        <TodoCreate />
+        <TodoCreate onCreateTodo={createTodo} />
         <TodoList />
       </div>
     </div>
--- src/components/TodoCreate.jsx
+++ src/components/TodoCreate.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/TodoCreate.css'
 
 
-function TodoCreate() {
+function TodoCreate({ onCreateTodo }) {
+
+    const [newTodo, setNewTodo] = useState('');
+
+    const createATodo = () => {
+        if (!newTodo) return;
+        const request = {
+            id: Math.floor(Math.random() * 1000),
+            title: newTodo
+        }
+        onCreateTodo(request)
+
+        setNewTodo('')
+    }
+
 
     return (
         <div className='todo-create'>
 
-            <input className='todo-input' type='text' placeholder='Todo Giriniz' />
-            <button className='todo-create-button'>Todo Oluştur</button>
+            <input value={newTodo} onChange={(e) => setNewTodo(e.target.value)} className='todo-input' type='text' placeholder='Todo Giriniz' />
+            <button onClick={createATodo} className='todo-create-button'>Todo Oluştur</button>
         </div>
     )
 }
